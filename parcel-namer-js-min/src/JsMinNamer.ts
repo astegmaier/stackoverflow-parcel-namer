d3938a63c1adc4fcb106d85c4d2bdcb518805c46
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-10-11 12:35:05 -0400

    Make the namer actually do something.

--- parcel-namer-js-min/src/JsMinNamer.ts
+++ parcel-namer-js-min/src/JsMinNamer.ts
@@ -1,9 +1,20 @@
 import { Namer } from "@parcel/plugin";
+import path from "path";
 
 export default new Namer({
-  name({ bundle, logger }) {
-    const filePath = bundle.getMainEntry()?.filePath;
-    logger.info({ origin: "parcel-namer-js-min", message: `FilePath is: ${filePath}` });
+  name({ bundle }) {
+    if (bundle.type === "js") {
+      const filePath = bundle.getMainEntry()?.filePath;
+      if (filePath) {
+        let baseNameWithoutExtension = path.basename(filePath, path.extname(filePath));
+        // See: https://parceljs.org/plugin-system/namer/#content-hashing
+        if (!bundle.needsStableName) {
+          baseNameWithoutExtension += "." + bundle.hashReference;
+        }
+        return `${baseNameWithoutExtension}.min.js`;
+      }
+    }
+    // Returning null means parcel will keep the name of non-js bundles the same.
     return null;
   },
 });
